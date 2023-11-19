38318bf50be153e2f398323b2a29b87ca14eb693
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-20 00:36:33 +0530

    added images and features to play video

--- src/app/MainHome/page.tsx
+++ src/app/MainHome/page.tsx
@@ -9,12 +9,28 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Image from "next/image";
 import image1 from "../assets/n1.jpg";
+import image2 from "../assets/n3.jpg";
+import image3 from "../assets/n2.jpg";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ReactPlayer from "react-player/youtube";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 export default function Page() {
   const [isSidebar, setSidebar] = useState(false);
-
   function handleToggleSidebar() {
     setSidebar(!isSidebar);
+  }
+
+  const [open, setOpen] = useState(false);
+  function handleClickOpenModal() {
+    setOpen(!open);
+  }
+
+  function handleClickCloseModal() {
+    setOpen(false);
+    // console.log("HI");
   }
 
   return (
@@ -89,13 +105,73 @@ export default function Page() {
           </Avatar>
         </div>
       </div>
-      <div className="border-2 overflow-hidden">
-        <Image src={image1} className="w-2/6" alt="" />
 
-        {/* <Image src="" alt="" />
-        <Image src="" alt="" />
-        <Image src="" alt="" />
-        <Image src="" alt="" /> */}
+      <div className="flex items-center overflow-hidden">
+        <Image src={image1} className="w-2/6 max-lg:hidden" alt="" />
+        <div className="relative">
+          <Image src={image2} className="w-full max-xl:w-full object-fill max-lg:w-full max-lg:object-cover" alt="" />
+          <div className="absolute bottom-0 left-0 right-0 p-6">
+            <h1 className="text-6xl text-slate-100 font-extrabold max-lg:text-5xl">
+              John Wick
+            </h1>
+            <div className="flex gap-3 mt-4">
+              <p className="text-xl font-semibold">New</p>
+              <button className="px-2  bg-gray-200 text-black font-semibold">
+                18 +
+              </button>
+              <button className="px-2  bg-gray-200 text-black font-semibold">
+                HD
+              </button>
+              <button className="px-2 bg-gray-200 text-black font-semibold">
+                cc
+              </button>
+            </div>
+            <p className="mt-5 text-xl font-semibold ">Movie 1hr 41m</p>
+            <div className="flex gap-4 mt-4">
+              <button
+                onClick={handleClickOpenModal}
+                className="flex items-center gap-1 bg-gray-100 font-semibold text-2xl px-10 py-1 text-black rounded-sm max-lg:text-xl max-lg:px-3"
+              >
+                <PlayArrowIcon /> Play
+              </button>
+              <Modal
+                keepMounted
+                open={open}
+                onClose={handleClickCloseModal}
+                aria-labelledby="keep-mounted-modal-title"
+                aria-describedby="keep-mounted-modal-description"
+              >
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: "85%",
+                    height: "85%",
+                    "@media screen and (max-width: 1024px)": {
+                      width: "90%",
+                      height: "50%",
+                    },
+                  }}
+                >
+                  <ReactPlayer
+                    url="https://youtu.be/qEVUtrk8_B4?si=MAB2uxVyy9_dk6x5"
+                    controls={true}
+                    playing={false}
+                    width="100%"
+                    height="100%"
+                  />
+                </Box>
+              </Modal>
+
+              <button className="flex items-center gap-1 text-2xl font-semibold bg-red-700 px-5 py-1 text-white rounded-sm max-lg:text-xl max-lg:px-3">
+                <InfoOutlinedIcon /> More info
+              </button>
+            </div>
+          </div>
+        </div>
+        <Image src={image3} className="w-2/6 max-lg:hidden" alt="" />
       </div>
     </Fragment>
   );
