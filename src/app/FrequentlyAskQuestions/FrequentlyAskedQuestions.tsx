4374dc85db79c309199c5f12363e56ad59ac3259
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-19 09:49:10 +0530

    animation one in frequently asked questions section

--- src/app/FrequentlyAskQuestions/FrequentlyAskedQuestions.tsx
+++ src/app/FrequentlyAskQuestions/FrequentlyAskedQuestions.tsx
@@ -24,122 +24,32 @@ export function FrequentlyAskedQuestions() {
             Frequently Asked Questions
           </h1>
         </div>
-        <div className="mt-10 ">
-          <ul className="list:none p-2 flex flex-col gap-4">
-            <li>
-              <label
-                htmlFor="first"
-                className=" cursor-pointer bg-neutral-900 flex items-center justify-start w-3/4 m-auto p-6 text-3xl  text-center max-lg:w-11/12 max-lg:text-lg"
-              >
-                What is Streamis ?
-              </label>
-              <div className="text-justify">
-                <p className="hidden">
-                  Streamis is a streaming service that offers a wide variety of
-                  award-winning TV shows, movies, anime, documentaries and more
-                  – on thousands of internet-connected devices. <br /> <br />
-                  You can watch as much as you want, whenever you want, without
-                  a single ad – all for one low monthly price. There is always
-                  something new to discover, and new TV shows and movies are
-                  added every week!
-                </p>
-              </div>
-            </li>
 
-            <li>
-              <label
-                htmlFor="second"
-                className="cursor-pointer bg-neutral-900 flex items-center justify-start w-3/4 m-auto p-6 text-3xl  text-center max-lg:w-11/12 max-lg:text-lg"
-              >
-                How much does Streamis cost ?
-              </label>
-              <div className="hidden">
-                <p>
-                  Watch Streamis on your smartphone, tablet, Smart TV, laptop,
-                  or streaming device, all for one fixed monthly fee. Plans
-                  range from ₹ 100 to ₹ 400 a month. No extra costs, no
-                  contracts.
-                </p>
-              </div>
-            </li>
-
-            <li>
-              <label
-                htmlFor="third"
-                className="cursor-pointer bg-neutral-900 flex items-center justify-start w-3/4 m-auto p-6 text-3xl  text-center max-lg:w-11/12 max-lg:text-lg"
-              >
-                Where can I watch ?
-              </label>
-              <div className="hidden">
-                <p>
-                  Watch anywhere, anytime. Sign in with your Streamis account to
-                  watch instantly on the web at streamis.com from your personal
-                  computer or on any internet-connected device that offers the
-                  Streamis app, including smart TVs, smartphones, tablets,
-                  streaming media players and game consoles.
-                  <br /> <br />
-                  You can also download your favourite shows with the iOS,
-                  Android, or Windows 10 app. Use downloads to watch while you
-                  are on the go and without an internet connection. Take
-                  Streamis with you anywhere.
-                </p>
-              </div>
-            </li>
-
-            <li>
-              <label
-                htmlFor="fourth"
-                className="cursor-pointer bg-neutral-900 flex items-center justify-start w-3/4 m-auto p-6 text-3xl  text-center max-lg:w-11/12 max-lg:text-lg"
-              >
-                How do I cancel ?
-              </label>
-              <div className="hidden">
-                <p>
-                  Streamis is flexible. There are no annoying contracts and no
-                  commitments. You can easily cancel your account online in two
-                  clicks. There are no cancellation fees – start or stop your
-                  account anytime.
-                </p>
-              </div>
-            </li>
-
-            <li>
-              <label
-                htmlFor="fifth"
-                className="cursor-pointer bg-neutral-900 flex items-center justify-start w-3/4 m-auto p-6 text-3xl  text-center max-lg:w-11/12 max-lg:text-lg"
-              >
-                What I can watch on Streamis ?
-              </label>
-              <div className="hidden">
-                <p>
-                  Streamis has an extensive library of feature films,
-                  documentaries, TV shows, anime, award-winning Streamis
-                  originals, and more. Watch as much as you want, anytime you
-                  want.
-                </p>
-              </div>
-            </li>
-
-            <li>
-              <label
-                htmlFor="sixth"
-                className="cursor-pointer bg-neutral-900 flex items-center justify-start w-3/4 m-auto p-6 text-3xl  text-center max-lg:w-11/12 max-lg:text-lg"
-              >
-                Is Streamis good for kids ?
-              </label>
-              <div className="hidden">
-                <p>
-                  The Netflix Kids experience is included in your membership to
-                  give parents control while kids enjoy family-friendly TV shows
-                  and films in their own space.
-                  <br /> <br />
-                  Kids profiles come with PIN-protected parental controls that
-                  let you restrict the maturity rating of content kids can watch
-                  and block specific titles you don’t want kids to see.
-                </p>
-              </div>
-            </li>
-          </ul>
+        <div className="mt-10 w-4/5 m-auto">
+          <Accordion
+            sx={{ backgroundColor: "#2c2c2c" }}
+            expanded={expanded === "panel1"}
+            onChange={handleChange("panel1")}
+          >
+            <AccordionSummary
+              expandIcon={<KeyboardArrowDownIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ color: "white" }}>What is Streamis</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography sx={{ color: "white" }}>
+                Streamis is a streaming service that offers a wide variety of
+                award-winning TV shows, movies, anime, documentaries and more –
+                on thousands of internet-connected devices. <br /> <br />
+                You can watch as much as you want, whenever you want, without a
+                single ad – all for one low monthly price. There is always
+                something new to discover, and new TV shows and movies are added
+                every week!
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
 
         <div className="flex flex-col justify-center">
