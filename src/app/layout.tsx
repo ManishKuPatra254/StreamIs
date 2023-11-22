b6a54597279c2a91928dd0a99c2814b59ab81a2d
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-22 11:05:29 +0530

    final bugs fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/public/8103873.png" type="image/x-icon" />
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
